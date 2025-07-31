7794de86ccf7d433f08384d8e22bd8e1c50afe14
“officialHAX007git config --global user.email “u6632753@au.edu <your@email.com>
2025-07-31 10:16:58 +0700

    Fix: update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/midterm-quotation-app/', // ✅ VERY IMPORTANT
+  base: '/wad_mid/', // ✅ MUST MATCH REPO NAME EXACTLY!
   plugins: [react()],
 });
